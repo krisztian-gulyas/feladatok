3d2b6c20ef34b768162c935e012820a76b7bd7ca
krisztian-gulyas <84473765+krisztian-gulyas@users.noreply.github.com>
2021-10-28 10:12:38 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="hu">
+	<head>
+		<title>Sitebuilder-probamunka-7</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link rel="stylesheet" type="text/css" href="base.css">
+		<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+		<script type="text/javascript" src="bootstrap.min.js"></script>
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="content" class="w-100">
+				<div id="header" class="w-75 mx-auto h-auto mb-5">
+					<div id="search" class="w-30 float-end my-4">
+						<input type="text" name="searchSite" placeholder="Site Search">
+						<div class="btn">Go!</div>
+					</div>
+					<div>
+						<div class="h1">Smart</div>
+						<span>inspiring innovations</span>
+					</div>
+				</div>
+				<div id="container" class="w-auto mx-auto">
+					<div class="desc">
+						<h1 class="py-2 px-4">Only individual</h1>
+						<h4 class="py-2">customer support</h4>
+					</div>
+					<div class="csempe w-75 mx-auto">
+						<div class="cs_1 w-20">
+							<div class="header pt-4">
+								<h1>A</h1>
+								<h5>solutions</h5>
+							</div>
+							<div class="cs_desc pt-2">
+								Lalesua kertase meyuade
+								Odioner grda aselerade
+								magnrem bus jertyadesede
+								saleget kelems.
+							</div>
+							<div class="btn pt-2 pb-3">more</div>
+						</div>
+						<div class="cs_2 w-20">
+							<div class="header pt-4">
+								<h1>B</h1>
+								<h5>analysis</h5>
+							</div>
+							<div class="cs_desc pt-2">
+								Lalesua kertase meyuade
+								Odioner grda aselerade
+								magnrem bus jertyadesede
+								saleget kelems.
+							</div>
+							<div class="btn pt-2 pb-3">more</div>
+						</div>
+						<div class="cs_3 w-20">
+							<div class="header pt-4">
+								<h1>C</h1>
+								<h5>strategies</h5>
+							</div>
+							<div class="cs_desc pt-2">
+								Lalesua kertase meyuade
+								Odioner grda aselerade
+								magnrem bus jertyadesede
+								saleget kelems.
+							</div>
+							<div class="btn pt-2 pb-3">more</div>
+						</div>
+						<div class="cs_4 w-20">
+							<div class="header pt-4">
+								<h1>D</h1>
+								<h5>development</h5>
+							</div>
+							<div class="cs_desc pt-2">
+								Lalesua kertase meyuade
+								Odioner grda aselerade
+								magnrem bus jertyadesede
+								saleget kelems.
+							</div>
+							<div class="btn pt-2 pb-3">more</div>
+						</div>
+					</div>
+					<div class="bub mx-auto">
+						Lalesua kertase meyuade
+						Odioner grda aselerade
+						magnrem bus jertyadesede
+						saleget kelems. Lalesua kertase meyuade
+						Odioner grda aselerade
+						magnrem bus jertyadesede
+						saleget kelems. Lalesua kertase meyuade
+						Odioner grda aselerade
+						magnrem bus jertyadesede
+						saleget kelems.
+					</div>
+
+					<img src="icon-alfa.png" style="margin: 2% 0 2% 20%">
+				</div>
+				<div id="footer"></div>
+			</div>
+		</div>
+	</body>
+</html>
